a8ed2341a547225fb708ed95d38fdf6ffd4e3209
Kaue Pereira <kauefontes@outlook.com>
2022-10-16 13:53:18 -0400

    Added progress bar animation based on cards list

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 import { styles } from './styles';
 
@@ -9,19 +10,42 @@ type Props = {
 }
 
 export function Progress({ currentCard, totalOfCards }: Props) {
+  const [width, setwidth] = useState(0)
+  const animated = useSharedValue(-1000)
+
+  const animatedProgressBar = useAnimatedStyle(() => {
+    return {
+      transform: [{
+        translateX: animated.value
+      }]
+    }
+  })
+  useEffect(() => {
+    const currentProgressBarWidth = -width + (width * currentCard) / (totalOfCards + 1)
+    if (currentProgressBarWidth < 30) {
+      animated.value = withTiming(currentProgressBarWidth, { duration: 300 })
+    }
+  }, [currentCard, width])
+
   return (
     <View style={styles.container}>
       <View style={styles.percentage}>
         <Text style={styles.label}>0%</Text>
 
         <View style={styles.thumbProgressBar}>
-          <View style={styles.currentProgressBar} />
+          <Animated.View
+            style={[styles.currentProgressBar, animatedProgressBar]}
+            onLayout={(e) => {
+              const currentWidth = e.nativeEvent.layout.width
+              setwidth(currentWidth)
+            }}
+          />
         </View>
 
         <Text style={styles.label}>100%</Text>
       </View>
 
-      <Text style={styles.label}>{currentCard} de {totalOfCards} cards</Text>
+      <Text style={styles.label}>{currentCard} de {totalOfCards + 1} cards</Text>
     </View >
   );
 }
